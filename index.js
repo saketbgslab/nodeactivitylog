5209e2918beda7f090f14ef23758303b3fff3fbc
saketbgslab <saket.barwal@gslab.com>
2018-03-15 11:25:12 +0530

    write and accepted events are fetched

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //const Azure = require('azure');
 const MsRest = require('ms-rest-azure');
 const config = require('./azconfig');
+const async  = require('async')
 const ResourceManagementClient = require('azure-arm-resource').ResourceManagementClient;
 var fs = require('fs');
 var util = require('util');
@@ -35,38 +36,60 @@ function startExec(err, credentials) {
     resourceClient = new ResourceManagementClient(credentials, subscriptionId);
     eventClient = monitoring.createEventsClient(credentials, baseURI);
 
+
     resourceClient.resources.listByResourceGroup(resourceGroupName, function(err, result, request, response){
 
         if(err) {return console.log()}
-        resList = result;
-        console.log('\n\n\t res ' + JSON.stringify(result, 0,4) );
+       // console.log('\n\n\t res ' + JSON.stringify(result, 0,4) );
         fs.writeFileSync('./imb-gsl/resList.json', JSON.stringify(result, 0,4), 'utf-8');
+        getResourceLogs(result);
         
     });
 
-    var params = {};
-    var MS_PER_MINUTE = 60000;
-    var durationInMinutes = 12;
-    var myStartDate = new Date(Date.now() - durationInMinutes * MS_PER_MINUTE);
-    params.startTime = myStartDate;
 
-    for(res in resList){
-        params.resourceUri = res[id]
-
-        eventClient.eventData.listEventsForResource(params, function(err, res){
-            if(err) return console.log(err);
-            console.log(res)
-            fs.writeFileSync('./eventLog2.json', JSON.stringify(res, 0,4), 'utf-8');
-        });
-    }
-    eventClient.eventData.listEvents(params , function(err, res){
-        if(err) return console.log(err);
-        console.log(res)
-        fs.writeFileSync('./eventLog2.json', JSON.stringify(res, 0,4), 'utf-8');
-    }) 
 }
 
-function getResourceLogs(){
-    resourceClient.resourceGroups.list(callback);
+function getResourceLogs(resList){
+    var params = {};
+    var MS_PER_MINUTE = 60000;
+    var durationInMinutes = 43200;
+    var myStartDate = new Date(Date.now() - durationInMinutes * MS_PER_MINUTE);
+    params.startTime = myStartDate;
+        
+    async.each(resList, function(item, callback){
+                params.resourceUri = item['id'];
+                eventClient.eventData.listEventsForResource(params, function(err, result){
+                    if(err) return callback(err, null);
+                    //console.log(result)
+                    fs.writeFileSync('./imb-gsl/'+item['name']+'.json', JSON.stringify(result, 0,4), 'utf-8');
+                    return callback(null, result)
+                });
+
+        }, function(err){
+            if(err){return console.error('\n\n\t Resource Events have produced errors:' + err)}
+
+            console.log('\n\n Event logs for all resources fetched successfully!!');
+            parseAfile();
+            
+        })
+}
+
+function parseAfile(){
+    var eventLogs = JSON.parse(fs.readFileSync('./imb-gsl/cache999.json', 'utf-8'));
+
+
+    var resEventsArray = eventLogs['eventDataCollection']['value']
+    resEventsArray = resEventsArray.reverse();
+
+    resEventsArray.forEach(function(element) {
+        if(element['operationName']['value'].endsWith("write")  && element['status']['value'] === 'Accepted'){
+            console.log('\n\t ' + element['caller']+ '\t' + element['status']['value'] + '\t' + element['operationId'] )
+            break;
+        }
+      });
+    
+
+
+    
 }
 
